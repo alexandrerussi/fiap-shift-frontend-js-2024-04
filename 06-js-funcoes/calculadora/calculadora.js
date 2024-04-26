f3743b63a524269e5996451cd9055bc49dc4e4ab
inovationacademy <alexandrerussijunior@gmail.com>
2024-04-25 22:24:55 -0300

    objetos

--- 06-js-funcoes/calculadora/calculadora.js
+++ 06-js-funcoes/calculadora/calculadora.js
@@ -7,3 +7,37 @@ document.getElementById("somar").addEventListener("click", () => {
     let pResultado = document.getElementById("resultado");
     pResultado.innerHTML = `O resultado da soma é: ${soma}`;
 });
+
+
+function subtrair() {
+    let numA = +document.getElementById("primeiro-numero").value;
+    let numB = +document.getElementById("segundo-numero").value;
+
+    let subtracao = numA - numB;
+
+    document.getElementById("resultado").innerHTML = `O resultado da subtração é: ${subtracao}`;
+}
+
+
+document.getElementById("multiplicar").addEventListener("click", () => {
+    let numA = +document.getElementById("primeiro-numero").value;
+    let numB = +document.getElementById("segundo-numero").value;
+
+    let multi = numA * numB;
+
+    document.getElementById("resultado").innerHTML = `O resultado da multiplicação é: ${multi}`;
+});
+
+function dividir() {
+    let numerador = +document.getElementById("primeiro-numero").value;
+    let denominador = +document.getElementById("segundo-numero").value;
+
+    let retornoMsg;
+    if (denominador != 0) {
+        retornoMsg = `O resultado da divisão é ${numerador / denominador}`;
+    } else {
+        retornoMsg = "ERRO! Você não pode dividir por zero.";
+    }
+
+    document.getElementById("resultado").innerHTML = retornoMsg;
+}
